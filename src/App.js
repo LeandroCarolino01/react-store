c3852f3daa0068439a85d4ad3110299f3524121b
Leandro.Carolino <carolino.leandro@gmail.com>
2023-05-26 15:39:50 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import NavbarComponent from './components/Navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <NavbarComponent>
+      
+    </NavbarComponent>
+  );
+}
+
+export default App;
